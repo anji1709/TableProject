4eedfd994fcd377282654af9f07671bce7f1982c
anji1709 <anjaliverma1709@gmail.com>
2020-07-30 00:36:36 -0400

    Added a table list

---
+++ table.java
@@ -0,0 +1,14 @@
+class table
+{ 
+   public static void main(String hk[])
+   { 
+         for(int i=1;i<=10;i++)
+		 { 
+		    for(int j=1;j<=10;j++)
+			{ 
+			    System.out.println(i*j);
+			}
+			  System.out.println("         ");
+		 }
+   }
+}
